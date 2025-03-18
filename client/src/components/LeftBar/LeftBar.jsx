c25cabdcd9bf99f0ffc7ca7af6ddcdc161d80de7
Diego Vilhalva <diegorocha450@yahoo.com>
2025-03-18 11:19:16 -0300

    profle page create design

--- client/src/components/LeftBar/LeftBar.jsx
+++ client/src/components/LeftBar/LeftBar.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router";
 import Image from "../Image/Image";
 import "./LeftBar.css";
 
@@ -5,25 +6,25 @@ const LeftBar = () => {
     return (
         <div className="leftbar">
             <div className="menu-icons">
-                <a href="/" className="menu-icon" >
+                <Link to="/" className="menu-icon" >
                     <Image path="/general/logo.png" alt="PinSphere Logo" className="logo"/>
-                </a>
-                <a href="/" className="menu-icon active">
+                </Link>
+                <Link to="/" className="menu-icon active">
                     <Image path="/general/home.svg" alt="Home" />
-                </a>
-                <a href="/" className="menu-icon">
+                </Link>
+                <Link to="/create" className="menu-icon">
                     <Image path="/general/create.svg" alt="Create" />
-                </a>
-                <a href="/" className="menu-icon">
+                </Link>
+                <Link to="/" className="menu-icon">
                     <Image path="/general/updates.svg" alt="Updates" />
-                </a>
-                <a href="/" className="menu-icon">
+                </Link>
+                <Link to="/" className="menu-icon">
                     <Image path="/general/messages.svg" alt="Messages" />
-                </a>
+                </Link>
             </div>
-                <a href="/" className="menu-icon">
+                <Link to="/" className="menu-icon">
                     <Image path="/general/settings.svg" alt="Settings" />
-                </a>
+                </Link>
         </div>
     );
 };
--- client/src/routes/ProfilePage/ProfilePage.jsx
+++ client/src/routes/ProfilePage/ProfilePage.jsx
@@ -1,8 +1,30 @@
 import "./ProfilePage.css"
-
+import Image from "../../components/Image/Image"
+import { useState } from "react"
+import Collections from "../../components/Collections/Collections"
+import Gallery from "../../components/Gallery/Gallery"
 const ProfilePage = () => {
+  const [type, setType] = useState("saved")
   return (
-    <div>ProfilePage</div>
+    <div className="profile-page">
+      <Image path="/general/noAvatar.png" className="profile-img" alt='Profile picture' />
+      <h1 className="profile-name">John Doe</h1>
+      <span className="profile-username">@jhondoe</span>
+      <div className="follow-counts"> 10 Followers - 20 Following</div>
+      <div className="profile-interactions">
+        <Image path="/general/share.svg" alt="Share" />
+        <div className="profile-buttons">
+          <button>Mesage</button>
+          <button>Follow</button>
+        </div>
+        <Image path="/general/more.svg" alt="" />
+      </div>
+      <div className="profile-options">
+        <span className={type === "created" ? "active " : ""} onClick={() => setType("created")}>Created</span>
+        <span className={type === "saved" ? "active " : ""} onClick={() => setType("saved")}>Saved</span>
+      </div>
+      {type === "created" ? <Gallery/> : <Collections/>}
+    </div>
   )
 }
 
