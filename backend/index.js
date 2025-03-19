7a1207f6aa6e80c68d2c7f7f39412d103a190794
Diego Vilhalva <diegorocha450@yahoo.com>
2025-03-19 09:45:19 -0300

    client side setup

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+import  express from "express"
+import userRouter from "./routes/user.route.js"
+
+const app = express()
+
+
+app.use("/users",userRouter)
+
+
+
+
+app.listen(3000, () => {
+    console.log("Serving is running")
+})
