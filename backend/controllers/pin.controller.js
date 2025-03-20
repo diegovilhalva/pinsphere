833a32fdcef6d96215d2d3fc4ac28032c86f5575
Diego Vilhalva <diegorocha450@yahoo.com>
2025-03-20 11:30:28 -0300

    infinite scroll implemented

--- backend/controllers/pin.controller.js
+++ backend/controllers/pin.controller.js
@@ -3,8 +3,23 @@ import Pin from "../models/pin.model.js"
 
 export const getPins = async (req, res) => {
     try {
+        const limit = 21;
+        const cursor = parseInt(req.query.cursor) || 0;
+
         const pins = await Pin.find()
-        res.status(200).json({ success: true, data: pins })
+            .sort({ createdAt: -1 })
+            .skip(cursor * limit)
+            .limit(limit + 1);
+
+        const hasNextPage = pins.length > limit;
+        const results = hasNextPage ? pins.slice(0, -1) : pins;
+
+        await new Promise(resolve => setTimeout(resolve, 3000))
+        res.status(200).json({
+            success: true,
+            data: results,
+            nextCursor: hasNextPage ? cursor + 1 : null
+        });
     } catch (error) {
         console.error(error)
         res.status(500).json({
--- client/src/components/Gallery/Gallery.jsx
+++ client/src/components/Gallery/Gallery.jsx
@@ -1,24 +1,43 @@
 import GalleryItem from "../GalleryItem/GalleryItem";
 import "./Gallery.css"
-import { useQuery } from "@tanstack/react-query"
+import { useInfiniteQuery } from "@tanstack/react-query"
 import axios from "axios"
+import InfiteScroll from "react-infinite-scroll-component"
 const Gallery = () => {
 
-  const fetchPins = async() => {
-    const res = await axios.get(`${import.meta.env.VITE_API_ENDPOINT}/pins`)
-    return res.data.data
-  }
+  const fetchPins = async ({ pageParam = 0 }) => {
+    const res = await axios.get(`${import.meta.env.VITE_API_ENDPOINT}/pins`, {
+      params: { cursor: pageParam }
+    });
+    return res.data;
+  };
 
-  const {isPending,error,data} = useQuery({queryKey:['pins'],queryFn:fetchPins})
-  if (error) return "An error has occurred: " + error.message
-  if (isPending) return <p>Loading...</p>;
-  console.log(error)
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    status,
+    error
+  } = useInfiniteQuery({
+    queryKey: ['pins'],
+    queryFn: fetchPins,
+    initialPageParam: 0,
+    getNextPageParam: (lastPage) => lastPage.nextCursor,
+  });
+
+  const allPins = data?.pages.flatMap(page => page.data) || [];
+
+  if (status === "pending") return <div className="loading">Loading...</div>;
+  if (status === "error") return <div>Error: {error.message}</div>;
   return (
-    <div className="gallery">
-        {data.map(item => (
-            <GalleryItem key={item._id} item={item} />
+    <InfiteScroll dataLength={allPins.length} next={fetchNextPage} hasMore={!!hasNextPage} loader={<h3>Loading more posts</h3>} endMessage={<h3>All pins loaded</h3>}>
+      <div className="gallery">
+        {allPins.map(item => (
+          <GalleryItem key={item._id} item={item} />
         ))}
-    </div>
+      </div>
+    </InfiteScroll>
+
   )
 }
 
